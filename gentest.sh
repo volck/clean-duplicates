0a72b7c6c414638515d2e26397f6f328aaf2cf11
Emil Volckmar Ry <emil-volckmar.ry@norsk-tipping.no>
2024-11-06 15:29:17 +0100

    add echo to display progress

--- gentest.sh
+++ gentest.sh
@@ -5,9 +5,10 @@ echo "creating $1 files"
 
 NUMFILES=$1
 
+mkdir testfolder
 
 for i in $(seq $NUMFILES); do
-  echo $i 
+  echo "[$i/$NUMFILES]"
   size=$((RANDOM % 10 + 1))
   dd if=/dev/urandom of=testfolder/$(uuidgen) bs=100M count=$size
 done
