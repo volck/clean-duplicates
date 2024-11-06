44b84129c9d2436e9260f12ac07572b1fdfa948f
Emil Volckmar Ry <emil-volckmar.ry@norsk-tipping.no>
2024-11-06 15:26:31 +0100

    add wg for each calculator task

--- internal/calculator.go
+++ internal/calculator.go
@@ -17,11 +17,14 @@ func NewCalculator(thechan chan string) *Calculator {
 
 func (c *Calculator) Listen(inChan chan<- string, outChan chan<- File, wg *sync.WaitGroup) {
 	defer wg.Done()
+	Logger.Info("calculator listening")
+	var calculateWg sync.WaitGroup
 	for ch := range c.CalculateChan {
-		wg.Add(1)
-		go c.CalculateHash(ch, outChan, wg)
+		calculateWg.Add(1)
+		go c.CalculateHash(ch, outChan, &calculateWg)
 	}
-	Logger.Info("calculator done")
+	calculateWg.Wait()
+	close(outChan)
 }
 
 func (c *Calculator) CalculateHash(path string, outChan chan<- File, wg *sync.WaitGroup) {
@@ -29,6 +32,7 @@ func (c *Calculator) CalculateHash(path string, outChan chan<- File, wg *sync.Wa
 	theHash, err := calculateHash(path)
 	if err != nil {
 		Logger.Error("could not calculate hashes", slog.Any("error", err))
+		return
 	}
 	md5Hash := fmt.Sprintf("%x", *theHash)
 	f := File{FilePath: path, MD5Hash: md5Hash}
